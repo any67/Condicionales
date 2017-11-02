29673c0bf7a0bf6fb7ec07201a81a1da88d8a3d9
any67 <any_sf_1967@hotmail.com>
2017-11-02 20:47:48 -0300

    Creando clase Calificacion

---
+++ src/Calificacion.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author LAB08
+ */
+public class Calificacion 
+{
+     public static void main(String[] args)
+     {
+     float nota1;
+     float nota2;
+     float nota3;
+     float promedio=(nota1+nota2+nota3/3);
+     
+      nota1 = Float.parseFloat(JOptionPane.showInputDialog("Ingrese nota1"));
+      nota2 = Float.parseFloat(JOptionPane.showInputDialog("Ingrese nota2"));
+      nota3 = Float.parseFloat(JOptionPane.showInputDialog("Ingrese nota3"));
+      
+        
+
+
+         
+     }
+             
+}
