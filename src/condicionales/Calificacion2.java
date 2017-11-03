88adb3df79f7484f1dae7cfb157417914f6b4c81
any67 <any_sf_1967@hotmail.com>
2017-11-03 18:18:23 -0300

    creando clase calificacion2

---
+++ src/condicionales/Calificacion2.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package condicionales;
+import javax.swing.JOptionPane;
+
+
+/**
+ *
+ * @author LAB08
+ */
+public class Calificacion2 
+{
+    public static void main(String[] args)
+    {
+    float nota;
+    
+     nota = Float.parseFloat(JOptionPane.showInputDialog("Ingrese nota"));
+     
+        if (nota = 7) 
+        {
+           JOptionPane.showMessageDialog(null, "Notable");  
+        }
+        else
+    }        
+            
+}
